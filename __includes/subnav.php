7fbf6d3288b0d9576ff5c6b333a4866fd68d153d
keres237 <marianesoriano681@gmail.com>
2025-03-16 21:39:47 +0800

    Add files via upload

---
+++ __includes/subnav.php
@@ -0,0 +1,71 @@
+    <nav>
+        <section class="subnav-cont">
+            <div class="subnav-elements">
+                <a class="inter-extralight" id="Activities" href="">Activities, Projects, and Programs<img src="../imgs/Expand Arrow.png" alt=""></a>
+                <div id="Activities-dropdown" class="subnav-dropdown">
+                    <div class="dropdown-cont">
+                        <div class="dropdown-title">
+                            <div class="dropdown-content">
+                                <p class="inter-bold">Funded Research and Projects</p>
+                                <p class="inter-bold">Intellectual Property</p>
+                                <p class="inter-bold">Research Ethics</p>
+                                <p class="inter-bold">Research Dissemination</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="inter-extralight" id="E-Units" href="">Extension Units <img src="../imgs/Expand Arrow.png" alt=""></a>
+                <div id="Extensions_Units-dropdown" class="subnav-dropdown">
+                    <div class="dropdown-cont">
+                        <div class="dropdown-title">
+                            <div class="dropdown-content">
+                                <p class="inter-bold">Department of Extension Services and Community Development</p>
+                                <p class="inter-bold">Community Based Livelihood Education</p>
+                                <p class="inter-bold">Community Outreach and Development</p>
+                                <p class="inter-bold">Technology and Innovation Deployment</p>
+                                <p class="inter-bold">Extension Monitoring and Evaluation</p>
+                                <p class="inter-bold">Hands of Goodwill</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="inter-extralight" id="R-Centers" href="">Research Centers <img src="../imgs/Expand Arrow.png" alt=""></a>
+                <div id="Research_Centers-dropdown" class="subnav-dropdown">
+                <div class="dropdown-cont">
+                        <div class="dropdown-title">
+                            <div class="dropdown-content">
+                                <p class="inter-bold">Research Development and Evaluation Center</p>
+                                <p class="inter-bold">Research Ethics Oversight Center</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="inter-extralight" id="R-Office" href="">Research Office<img src="../imgs/Expand Arrow.png" alt=""></a>
+                <div id="Research_Office-dropdown" class="subnav-dropdown">
+                <div class="dropdown-cont">
+                        <div class="dropdown-title">
+                            <div class="dropdown-content">
+                            <p class="inter-bold">Intellectual Property & Technology</p>
+                            <p class="inter-bold">Business Management - Innovation and Technology Support Office</p>
+                            <p class="inter-bold">Research Centers Operation Office</p> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="inter-extralight" id="R-units" href="">Research Units<img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="Researach_Unit-dropdown" class="subnav-dropdown">
+                    <div class="dropdown-title">
+                        <div class="dropdown-content">
+                        <p class="inter-bold">Creatives Works Unit</p>
+                        <p class="inter-bold">Dissemination, Publication and Citation Monitoring Unit</p>
+                        <p class="inter-bold">Technology Business Incubation Unit</p>
+                        <p class="inter-bold">Statistics and Data Bank Unit</p>
+                        <p class="inter-bold">Research Project Development Unit</p>
+                        <p class="inter-bold">Monitoring, Evaluation and Consultancy Unit</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+    </section>
+</nav>  
