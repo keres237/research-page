b44111ad9d43d5f79090bbf4d8674d3a2051eefc
keres237 <marianesoriano681@gmail.com>
2025-03-16 21:37:28 +0800

    Add files via upload

---
+++ page-views/extension-views.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WMSU Extension Services</title>
+    <link rel="stylesheet" href="../css/extension.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+    <header>
+        <nav class="sub-nav">
+            <ul>
+                <li><a href="#">Activities, Projects, and Programs</a></li>
+                <li><a href="#">Extension Units</a></li>
+                <li><a href="#">Research Centers</a></li>
+                <li><a href="#">Research Offices</a></li>
+                <li><a href="#">Research Units</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="content">
+    <h2>Office Description</h2>
+    <p> The Western Mindanao State University (WMSU) University Extension reflects the  institution's commitment to adapting its extension programs to meet the evolving  needs of the region and align with contemporary best practices.</p>
+        
+    <h2>Vision</h2>
+     <p> By 2040, WMSU is a Smart Research University generating competent  professionals and global citizens engendered by the knowledge from sciences and  liberal education, empowering communities, promoting peace, harmony, and cultural  diversity.</p>
+        
+    <h2>Mission</h2>
+    <p>  WMSU commits to create a vibrant atmosphere of learning where science,  technology, innovation, research, the arts and humanities, and community  engagement flourish, and produce world-class professionals committed to sustainable  development and peace.</p>
+    
+    <button class="dropdown">Goals ▼</button>
+    <button class="dropdown">Objectives ▼</button>
+        
+    <h2>Extension Service Philosophy</h2>
+    <ul>
+        <p>WMSU Extension Services is guided by the philosophy that effectiveness of  services is measured by the improvement of the quality of life of the beneficiary  communities, as it: </p>
+            <p>1. Considers "Adopt a community" as an innovative strategy for change and  development; </p>
+            <p>2. Implement programs to address the basic needs of the beneficiary  communities;</p>
+            <p>3. Provides services for sustainable socio-economic development; </p>
+            <p>4. Forges with other agencies for stronger partnership, creating greater impact  on the lives of the people and community it serves.</p>
+    </ul>
+   
+    
+     <h2>Organizational Chart</h2>
+
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
---
+++ page-views/research-views.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WMSU - Research & Development</title>
+    <link rel="stylesheet" href="../css/research.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/subnav.css">
+</head>
+<body>
+<section class="hero-text-logo-container">
+        <div class="hero-section">
+            <div class="hero-text">
+                <p class="inter-bold">Research & <br>Development</p>
+            </div>
+            <img src="../imgs/WMSU-Logo.png" class="hero-logo-img img-fluid" alt="Logo">
+        </div>
+        <?php require_once '../__includes/subnav.php'; ?>
+    </section>
+</body>
+</html>
+<section class="content">
+    <h2 class="inter-bold">Office Description</h2>
+        <p>The mandate of Western Mindanao State University is to serve as an instrument for the promotion of socio-economic advancement of the various cultural communities in the region.  One of the organizational outcomes of WMSU is attaining improved higher education research  to promote economic productivity and innovation Research and Development. 
+        Aligned with the  new vision of WMSU in 2023 towards becoming a Smart Research University, along its mission  of fostering a vibrant learning environment where science, technology, innovation, research,  arts, humanities, and community engagement thrive, producing world-class professionals  dedicated to sustainable development and peace, the Research and Development policies of  WMSU is enhanced to support these strategic endeavors.</p>
+        
+    <h2 class="inter-bold">Vision</h2>
+        <p>The Research & Development Cluster aims to be the vital enabler of research and development services in WMSU for faculty and students to create socially responsive innovative research and  scientific breakthroughs that blend cutting-edge technologies with human-centric approaches,  
+        creating a harmonious and sustainable quality of life for individuals and industries.</p>
+        
+    <h2 class="inter-bold">Mission</h2>
+    <p> The Research & Development Cluster is committed to generating quality research, responsive  innovations, and cutting-edge technologies to meet the needs of both local and global  communities and industries served by the university through the conduct of quality research and  development programs</p>
+   
+    <button class="dropdown">Core Values ▼</button>   
+    <button class="dropdown">Goals ▼</button>
+    <button class="dropdown">Objectives ▼</button>
+        
+    <h2 class="inter-bold">Extension Service Philosophy</h2>
+        <ul>
+            <p>WMSU Extension Services is guided by the philosophy that effectiveness of  services is measured by the improvement of the quality of life of the beneficiary  communities, as it: </p>
+            <p>1. Considers "Adopt a community" as an innovative strategy for change and  development; </p>
+            <p>2. Implement programs to address the basic needs of the beneficiary  communities;</p>
+            <p>3. Provides services for sustainable socio-economic development; </p>
+            <p>4. Forges with other agencies for stronger partnership, creating greater impact  on the lives of the people and community it serves.</p>
+        </ul>
+    
+    <h2 class="inter-bold">Organizational Chart</h2>
+            <p>                 From 2024 on wards, the University Research Center has expanded its functions and initiated the  creation of R&D Centers specific to the field of academic disciplines in the university. </p>
+            <img src="../imgs/RDorg.jpeg" alt="Organizational Chart">
+</section>
+</body>
+</html>
